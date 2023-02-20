424871fd98d2961cf287e41ac5629d304e676c21
Sam van den Eijnden <vandenes@uoguelph.ca>
2023-02-20 00:31:05 -0500

    update hostname in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://seangle.github.io',
+    site: 'https://www.svde.xyz',
 });
